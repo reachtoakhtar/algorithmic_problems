b75027d32f2947e86d574f7b913f8b5d9f7b6d7d
Akhtar <reachtoakhtar@gmail.com>
2018-02-04 19:23:39 +0530

    - Added code for 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+import sys
+
+__author__ = "akhtar"
+
+
+def second_largest(lst):
+	first = -sys.maxsize - 1  # max=sys.maxsize
+	second = -sys.maxsize - 1
+
+	for element in lst:
+		if element > first:
+			second = first
+			first = element
+		elif element > second:
+			second = element
+
+	return second
+
+
+print(second_largest([1, 4, 2, -8, 90]))
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # algorithmic_problems
 This repository is for coding the algorithmic problems.
+
+Please refer the file for the code:
+
+1.py Find the 2nd largest element in an array.
