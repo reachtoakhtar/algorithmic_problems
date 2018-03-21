0d099dd1751a5b994e5fe7f2e5446fa93473d05a
Akhtar <reachtoakhtar@gmail.com>
2018-03-21 20:39:08 +0530

    - Added recursive binary search.

--- 5.py
+++ 5.py
@@ -2,7 +2,7 @@
 __author__ = "akhtar"
 
 
-def binary_search(array, item):
+def binary_search_iterative(array, item):
     first = 0
     last = len(array) - 1
     found = False
@@ -20,3 +20,17 @@ def binary_search(array, item):
                 first = midpoint + 1
 
     return midpoint if found else -1
+
+
+def binary_search_recursive(array, item, start, end):
+    if end - start + 1 < 0:
+        return -1
+    else:
+        midpoint = start + (end - start)//2
+        if array[midpoint] == item:
+            return midpoint
+        else:
+            if item < array[midpoint]:
+                return binary_search_recursive(array, item, start, midpoint-1)
+            else:
+                return binary_search_recursive(array, item, midpoint+1, end)
