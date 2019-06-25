ac89db46147996b48bf630d5043a99446b412a17
Akhtar <reachtoakhtar@users.noreply.github.com>
2019-06-25 20:25:54 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,17 @@
+s = "abc"
+o = ["abc", "acb", "cab", "bac", "bca", "cba"]
+
+
+def permute(s, i, length, lst):
+    if i == length:
+        lst.append(''.join(s))
+    else:
+        for j in range(i,length):
+            # Swap
+            s[i], s[j] = s[j], s[i]
+            permute(s, i+1, length, lst)
+            s[i], s[j] = s[j], s[i]
+
+    return lst
+
+print(permute(list(s), 0, len(s), []))
